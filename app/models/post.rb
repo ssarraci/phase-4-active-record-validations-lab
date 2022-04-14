a5117c0517070e63482d1cd718fa5fdea362f1aa
Storm <storm.sarracino01@gmail.com>
2022-04-14 12:15:38 -0500

    commit tree 3a79772b9168572045527be9a5fc8588c852fb1a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+   validates :title, presence: true
+   validates :content, length: { minimum: 250 }
+   validates :summary, length: { maximum: 250 }
+   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+   validate :clickbait
+
+   def clickbait
+      # possible_title = %w([Wont Believe Secret Top Guess])
+      unless self.title.match?(/(won't|believe|secret|top0-9|guess)/i)
+         self.errors.add(:title, "invalid title")
+      end
+   end
 end
